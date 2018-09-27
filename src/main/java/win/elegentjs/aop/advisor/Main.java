6b10ad69803fdeac13b75f0e395d85b10c68be84
liupj <MooN_2012@qq.com>
2018-09-27 20:51:42 +0800

    update

--- src/main/java/win/elegentjs/aop/advisor/Main.java
+++ src/main/java/win/elegentjs/aop/advisor/Main.java
@@ -1,15 +1,40 @@
 package win.elegentjs.aop.advisor;
 
+import org.springframework.aop.PointcutAdvisor;
+import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
+/**
+ *
+ * Spring AOP 要素
+ *
+ *  Advisor:
+ *
+ *      Advice
+ *      Pointcut
+ *
+ *  织入器：ProxyFactory
+ *
+ */
 public class Main {
 
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        ProxyFactory proxyFactory = (ProxyFactory) context.getBean("proxyFactory");
+        PointcutAdvisor advisor = (PointcutAdvisor) context.getBean("advisor");
+
+        proxyFactory.addAdvisor(advisor);
+
+
         PerformanceService performanceService = (PerformanceService) context.getBean("performanceService");
+
+        proxyFactory.setTarget(performanceService);
+
+        performanceService = (PerformanceService) proxyFactory.getProxy();
 
         performanceService.printService();
 
