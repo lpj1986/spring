3fb02cbe753057a45f6ec30dff14bcfa0efbaa8d
liupj <MooN_2012@qq.com>
2018-10-10 13:57:19 +0800

    update

--- src/main/java/win/elegentjs/jdbc/Main.java
+++ src/main/java/win/elegentjs/jdbc/Main.java
@@ -3,7 +3,6 @@ package win.elegentjs.jdbc;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 
-import javax.sql.DataSource;
 import java.util.List;
 import java.util.Map;
 
@@ -11,11 +10,7 @@ public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
 
-        DataSource dataSource = (DataSource) applicationContext.getBean("dataSource");
-
-
-        SimpleJdbcTemplate template = new SimpleJdbcTemplate(dataSource);
-
+        SimpleJdbcTemplate template = (SimpleJdbcTemplate) applicationContext.getBean("jdbcTemplate");
         List<Map<String, Object>> list = template.queryForList("select * from product");
 
         System.out.println(list);
