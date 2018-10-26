ff6fc275e544504f62d0dd9a40273f75864b4ab4
liupj <MooN_2012@qq.com>
2018-10-27 00:12:40 +0800

    update

---
+++ src/main/java/win/elegentjs/jdbc/datasource/RoutingDataSource.java
@@ -0,0 +1,24 @@
+package win.elegentjs.jdbc.datasource;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+/**
+ * 可动态改变的dataSource
+ *
+ * @author liupeijun
+ *
+ */
+public class RoutingDataSource extends AbstractRoutingDataSource {
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return DataSourceTypeManager.get();
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+    }
+}
