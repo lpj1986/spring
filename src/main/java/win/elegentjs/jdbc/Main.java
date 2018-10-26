f71a6773ce1df945bc26ba295b95ec85905c77ba
liupj <MooN_2012@qq.com>
2018-10-27 00:39:52 +0800

    update

--- src/main/java/win/elegentjs/jdbc/Main.java
+++ src/main/java/win/elegentjs/jdbc/Main.java
@@ -2,6 +2,8 @@ package win.elegentjs.jdbc;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import win.elegentjs.jdbc.datasource.DataSourceEnum;
+import win.elegentjs.jdbc.datasource.DataSourceTypeManager;
 
 import java.util.List;
 import java.util.Map;
@@ -9,9 +11,14 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-
         SimpleJdbcTemplate template = (SimpleJdbcTemplate) applicationContext.getBean("jdbcTemplate");
-        List<Map<String, Object>> list = template.queryForList("select * from product");
+
+        DataSourceTypeManager.set(DataSourceEnum.MASTER);
+        List<Map<String, Object>> list = template.queryForList("select * from T_BASE_RESOURCE");
+
+
+        DataSourceTypeManager.set(DataSourceEnum.SLAVE1);
+        list = template.queryForList("select * from T_BASE_RESOURCE");
 
         System.out.println(list);
 
