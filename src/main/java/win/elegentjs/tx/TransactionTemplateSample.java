d9666347863517b78840878febed12db500cdbea
liupj <MooN_2012@qq.com>
2018-10-26 16:01:36 +0800

    update

---
+++ src/main/java/win/elegentjs/tx/TransactionTemplateSample.java
@@ -0,0 +1,47 @@
+package win.elegentjs.tx;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TransactionTemplateSample {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        TransactionTemplate transactionTemplate = (TransactionTemplate) context.getBean("transactionTemplate");
+        DataSource dataSource = (DataSource) context.getBean("dataSource");
+
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                Connection conn = DataSourceUtils.getConnection(dataSource);
+
+                PreparedStatement pstmt;
+                try {
+                    pstmt = conn.prepareStatement("select * from T_BASE_RESOURCE");
+                    ResultSet rs = pstmt.executeQuery();
+
+                    while (rs.next()) {
+                        System.out.println(rs.getString(1));
+                    }
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+    }
+}
