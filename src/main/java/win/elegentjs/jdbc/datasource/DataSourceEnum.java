5f347d6dadb6627c83fd289b281384107c221a9d
liupj <MooN_2012@qq.com>
2018-10-26 23:34:57 +0800

    update

---
+++ src/main/java/win/elegentjs/jdbc/datasource/DataSourceEnum.java
@@ -0,0 +1,18 @@
+package win.elegentjs.jdbc.datasource;
+
+public enum DataSourceEnum {
+    /**
+     * 主库
+     */
+    MASTER,
+
+    /**
+     * 从库1
+     */
+    SLAVE1,
+
+    /**
+     * 从库2
+     */
+    SLAVE2
+}
