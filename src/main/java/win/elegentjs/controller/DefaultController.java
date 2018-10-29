6feec5538508415cbc03ff48e0135dacfc86d0c5
liupj <MooN_2012@qq.com>
2018-10-29 11:03:24 +0800

    update

--- src/main/java/win/elegentjs/controller/DefaultController.java
+++ src/main/java/win/elegentjs/controller/DefaultController.java
@@ -3,14 +3,13 @@ package win.elegentjs.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(path = "/index")
 public class DefaultController {
 
-
-    public ModelAndView index() {
-        return new ModelAndView("index");
+    @RequestMapping(path = "")
+    public String index() {
+        return "index";
     }
 }
